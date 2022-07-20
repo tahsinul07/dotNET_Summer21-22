40fc27297be028d703d9a07e08c8ae8b47050964
Hoque, Tahsinul <69600495+tahsinul07@users.noreply.github.com>
2022-07-20 22:17:44 +0600

    Add files via upload

---
+++ Vendor/Controllers/LoginVendorController.cs
@@ -0,0 +1,46 @@
+﻿using Grocery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grocery.Controllers
+{
+    public class LoginVendorController : Controller
+    {
+        db_vendorEntities db = new db_vendorEntities();
+        // GET: LoginVendor
+        [HttpGet]
+        public ActionResult LoginVendor()
+        {
+            return View();
+        }
+
+        public ActionResult LoginVendor(LoginViewModel model)
+        {
+            var user = (from v in db.tbl_vendor
+                        where v.Email.Equals(model.Email) &&
+                        v.Password.Equals(model.Password)
+                        select v).SingleOrDefault();
+            if (user != null)
+            {
+                ViewBag.SuccessMessage = "Rgistration Successful!";
+                Session["IdSS"] = user.BIN;
+                Session["NameSS"] = user.Name;
+                return RedirectToAction("VendorProfile","Vendor",user);
+                //return View();
+                //ViewBag.DuplicateMessage = "User with this email already exist";
+                //return View("Vendor", model);
+            }
+            ViewBag.NotFoundMessage = "User Not Found.";
+            return View("LoginVendor", new LoginViewModel());
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("LoginVendor","LoginVendor", new tbl_vendor());
+        }
+    }
+}
---
+++ Vendor/Controllers/VendorController.cs
@@ -0,0 +1,118 @@
+﻿using Grocery.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grocery.Controllers
+{
+    public class VendorController : Controller
+    {
+        db_vendorEntities db = new db_vendorEntities();
+        // GET: Vendor
+        [HttpGet]
+        public ActionResult Vendor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Vendor(tbl_vendor model)
+        {
+
+            if (db.tbl_vendor.Any(x=>x.Email == model.Email))
+            {
+                ViewBag.DuplicateMessage = "User with this email already exist";
+                return View("Vendor",model);
+            }
+            if(ModelState.IsValid)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(model.ImageFile.FileName);
+                string extension = Path.GetExtension(model.ImageFile.FileName);
+                fileName = fileName+ DateTime.Now.ToString("yymmssffff") + extension;
+                model.Photo = fileName;
+                fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                model.ImageFile.SaveAs(fileName);
+                tbl_vendor entity = new tbl_vendor();
+                entity.Name = model.Name;
+                entity.Password = model.Password;
+                entity.Address = model.Address;
+                entity.Email = model.Email;
+                entity.Mobile = model.Mobile;
+                entity.Photo = model.Photo;
+                entity.Password = model.Password;
+
+                db.tbl_vendor.Add(entity);
+                db.SaveChanges();
+
+                //db.tbl_vendor.Add(model);
+                //db.SaveChanges();
+
+
+                ModelState.Clear();
+                ViewBag.SuccessMessage = "Rgistration Successful!";
+                return RedirectToAction("LoginVendor", "LoginVendor", new LoginViewModel());
+            }
+            return View("LoginVendor", new tbl_vendor());
+        }
+
+        [HttpGet]
+        public ActionResult VendorProfile(tbl_vendor model)
+        {   
+            return View(model);
+        }
+        
+
+        public ActionResult EditProfile()
+        {
+            
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult EditProfile(tbl_vendor model)
+        {
+            var user = (from v in db.tbl_vendor
+                        where v.BIN.Equals(model.BIN)
+                        select v).SingleOrDefault();
+            if (model.Name != null && model.Email != null && model.Mobile != null && model.Address != null)
+            {
+                
+                user.Name = model.Name;
+                user.Email = model.Email;
+                user.Mobile = model.Mobile;
+                user.Address = model.Address;
+
+                Session["NameSS"] = user.Name;
+
+                db.SaveChanges();
+            }
+
+            return View("VendorProfile", user);
+        }
+
+        public ActionResult Edit()
+        {
+            return View() ;
+        }
+
+        public ActionResult DeleteVendor(tbl_vendor model)
+        {
+            var user = db.tbl_vendor.Where(x=>x.BIN == model.BIN).First();
+            db.tbl_vendor.Remove(user);
+            db.SaveChanges();
+
+
+            Session["IdSS"] = null;
+            Session["NameSS"] = null;
+            return RedirectToAction("LoginVendor", "LoginVendor");
+        }
+
+
+        
+    }
+}
